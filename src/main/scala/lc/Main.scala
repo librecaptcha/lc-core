fc48a1924ae0eac27997f20a00fd0e1e6577365e
Rahul Rudragoudar <rr83019@gmail.com>
2019-06-03 13:26:08 +0530

    Minor fixes
    Signed-off-by: Rahul Rudragoudar <rr83019@gmail.com>

--- src/main/scala/lc/Main.scala
+++ src/main/scala/lc/Main.scala
@@ -154,6 +154,7 @@ case class Size(height: Int, width: Int)
 case class Parameters(level: String, media: String, input_type: String, size: Option[Size])
 case class Id(id: String)
 case class Answer(answer: String, id: String)
+case class Secret(token: Int)
 
 class Server(port: Int){
 	val captcha = new Captcha(0)
