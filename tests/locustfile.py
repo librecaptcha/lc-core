42f908ae2c82dd5b94430cd3893b52ec16044ccd
Rahul Rudragoudar <rr83019@gmail.com>
2020-09-22 05:25:38 +0530

    Update locustfile
    Signed-off-by: Rahul Rudragoudar <rr83019@gmail.com>

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,48 +1,36 @@
-from locust import task, between
+from locust import task, between, SequentialTaskSet
 from locust.contrib.fasthttp import FastHttpUser
 import json
 import uuid
 
-class QuickStartUser(FastHttpUser):
+class QuickStartUser(SequentialTaskSet):
     wait_time = between(0.1,1)
 
     captcha_params = {"level":"some","media":"some","input_type":"some"}
     answerBody = {"answer": "qwer123"}
-    hash = ""
 
-    def on_start(self):
-        resp = self.client.get(path="/v1/token?email=%s" % str(uuid.uuid4()), name = "/register")
-        self.hash = str(json.loads(resp.text).get("token"))
- 
     @task
     def captcha(self):
-        resp = self.client.post(path="/v1/captcha", json=self.captcha_params, headers={"access-token":self.hash})
+        resp = self.client.post(path="/v1/captcha", json=self.captcha_params, name="/captcha")
         if resp.status_code != 200:
             print("\nError on /captcha endpoint: ")
             print(resp)
             print(resp.text)
             print("----------------END.C-------------------\n\n")
-        #when rate limit is reached
-        try:
-            self.answerBody["id"] = json.loads(resp.text).get("id")
-        except:
-            self.answerBody["id"] = "None"
-
+        self.answerBody["id"] = json.loads(resp.text).get("id")
 
     @task
     def media(self):
-        if self.answerBody.get("id") != "None":
-            resp = self.client.get(path="/v1/media?id=%s" % self.answerBody.get("id"), name="/media")
-            if resp.status_code != 200:
-                print("\nError on /media endpoint: ")
-                print(resp)
-                print(resp.text)
-                print("-----------------END.M-------------------\n\n")
-
+        resp = self.client.get(path="/v1/media?id=%s" % self.answerBody.get("id"), name="/media")
+        if resp.status_code != 200:
+            print("\nError on /media endpoint: ")
+            print(resp)
+            print(resp.text)
+            print("-----------------END.M-------------------\n\n")
 
     @task
     def answer(self):
-        resp = self.client.post(path='/v1/answer', json=self.answerBody)
+        resp = self.client.post(path='/v1/answer', json=self.answerBody, name="/answer")
         if resp.status_code != 200:
             print("\nError on /answer endpoint: ")
             print(resp)
