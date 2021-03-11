21b299a30fe0d4c499c04d02b88140b5411f6a3d
Rahul Rudragoudar <rr83019@gmail.com>
2021-03-11 21:26:30 +0530

    Add captchaConfig case class
    Signed-off-by: Rahul Rudragoudar <rr83019@gmail.com>

--- src/main/scala/lc/core/models.scala
+++ src/main/scala/lc/core/models.scala
@@ -1,7 +1,16 @@
 package lc.core
 
+sealed trait ChallengeResult
 case class Size(height: Int, width: Int)
 case class Parameters(level: String, media: String, input_type: String, size: Option[Size])
-case class Id(id: String)
+case class Id(id: String) extends ChallengeResult
+case class Error(message: String) extends ChallengeResult
 case class Answer(answer: String, id: String)
 case class Result(result: String)
+case class CaptchaConfig(
+    name: String,
+    allowedLevels: List[String],
+    allowedMedia: List[String],
+    allowedInputType: List[String],
+    config: String
+)
