f02eadb945cb94d6344a5f52faac2ec15bf1057f
hrj <harshad.rj@gmail.com>
2021-04-02 15:51:55 +0530

    specify run time of locust tests

--- tests/run.sh
+++ tests/run.sh
@@ -5,6 +5,6 @@ java -jar target/scala-2.13/LibreCaptcha.jar &
 JAVA_PID=$!
 sleep 4
 
-locust --headless -u 1000 -r 100 -f tests/locustfile.py
+locust --headless -u 1000 -r 100 --run-time 4m --stop-timeout 30 -f tests/locustfile.py
 
 kill $JAVA_PID
