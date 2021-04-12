de4a853452d1f23185343f3ac4ed6fc2e411bd09
Rahul Rudragoudar <rr83019@gmail.com>
2021-04-12 23:12:54 +0530

    Update models
    Signed-off-by: Rahul Rudragoudar <rr83019@gmail.com>

--- src/main/scala/lc/core/models.scala
+++ src/main/scala/lc/core/models.scala
@@ -6,7 +6,7 @@ case class Parameters(level: String, media: String, input_type: String, size: Op
 case class Id(id: String) extends ChallengeResult
 case class Image(image: Array[Byte]) extends ChallengeResult
 case class Answer(answer: String, id: String)
-case class Result(result: String)
+case class Result(result: String) extends ChallengeResult
 case class Error(message: String) extends ChallengeResult
 case class Response(statusCode: Int, message: Array[Byte])
 case class CaptchaConfig(
