3845645f9a4b3e2c286054cc5b0e917f11645223
hrj <harshad.rj@gmail.com>
2021-04-13 09:05:44 +0530

    use debug config
    Signed-off-by: hrj <harshad.rj@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -11,13 +11,23 @@ locust --headless -u 300 -r 100 --run-time 4m --stop-timeout 30 -f tests/locustf
 status=$?
 
 if [ $status != 0 ]; then
-  kill $JAVA_PID
   exit $status
 fi
 
+kill $JAVA_PID
+sleep 4
+
 echo Run functional test
+cp data/config.json data/config.json.bak
+cp tests/debug-config.json data/config.json
+
+java -jar target/scala-2.13/LibreCaptcha.jar &
+JAVA_PID=$!
+sleep 4
+
 locust --headless -u 1 -r 1 --run-time 1m --stop-timeout 30 -f tests/locustfile-functional.py
 status=$?
+mv data/config.json.bak data/config.json
 
 kill $JAVA_PID
 exit $status
