51a1f0a71030238ea1b74fa45bb66c8aaa4dfa68
hrj <harshad.rj@gmail.com>
2021-12-10 00:01:35 +0530

    update h2 to 2.0.202
    Note that this version is NOT backwards compatible with 1.x version
    
    Hence, we use a new DB path "captcha2". The contents in the old DB can be reused
    by following instructions in
    https://github.com/h2database/h2database/releases/tag/version-2.0.202

--- src/main/scala/lc/database/DB.scala
+++ src/main/scala/lc/database/DB.scala
@@ -3,7 +3,7 @@ package lc.database
 import java.sql._
 
 class DBConn() {
-  val con: Connection = DriverManager.getConnection("jdbc:h2:./data/H2/captcha", "sa", "")
+  val con: Connection = DriverManager.getConnection("jdbc:h2:./data/H2/captcha2", "sa", "")
 
   def getStatement(): Statement = {
     con.createStatement()
