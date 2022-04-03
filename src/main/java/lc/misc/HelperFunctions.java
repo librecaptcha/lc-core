6480da09ffd76102716269b913105ee0210e5dbe
hrj <harshad.rj@gmail.com>
2022-04-03 23:01:26 +0530

    helper setRenderingHints: set antialiasing on

--- src/main/java/lc/misc/HelperFunctions.java
+++ src/main/java/lc/misc/HelperFunctions.java
@@ -18,6 +18,7 @@ public class HelperFunctions {
   }
 
   public static void setRenderingHints(Graphics2D g2d) {
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     g2d.setRenderingHint(
         RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
     g2d.setRenderingHint(
